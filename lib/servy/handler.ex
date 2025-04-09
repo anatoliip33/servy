f51fed02941577de1ba907d90e3cd3943c125d87
anatolii <anatoliipolishchuk@gmail.com>
2025-04-09 15:31:48 +0300

    27. Another GenServer

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -4,7 +4,6 @@ defmodule Servy.Handler do
   """
   alias Servy.Conv
   alias Servy.BearController
-  alias Servy.VideoCam
 
   import Servy.Plugins, only: [rewrite_path: 1, track: 1]
   import Servy.Parser, only: [parse: 1]
@@ -31,16 +30,9 @@ defmodule Servy.Handler do
   end
 
   def route(%Conv{ method: "GET", path: "/sensors" } = conv) do
-    task = Task.async(fn -> Servy.Tracker.get_location("bigfoot") end)
+    sensor_data = Servy.SensorServer.get_sensor_data()
 
-    snapshots =
-      ["cam-1", "cam-2", "cam-3"]
-      |> Enum.map(&Task.async(fn -> VideoCam.get_snapshot(&1) end))
-      |> Enum.map(&Task.await/1)
-
-    where_is_bigfoot = Task.await(task)
-
-    %{ conv | status: 200, resp_body: inspect {snapshots, where_is_bigfoot} }
+    %{ conv | status: 200, resp_body: inspect sensor_data }
   end
 
   def route(%Conv{ method: "GET", path: "/kaboom" }) do
